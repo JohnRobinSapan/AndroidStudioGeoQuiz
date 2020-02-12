f73b73071edaa7fb3798994a324ae6b85e446fd5
John Sapan <sapanjo@shernet.sheridancollege.ca>
2020-02-11 21:15:48 -0500

    ViewModel and landscape layout added

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,35 +1,37 @@
 package com.example.geoquiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
-import android.widget.*
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: ImageButton
-    private lateinit var prevButton: ImageButton
+    private lateinit var nextButton: Button
+    private lateinit var prevButton: Button
     private lateinit var questionTextView: TextView
-    private lateinit var geoImageView : ImageView
+    private lateinit var geoImageView: ImageView
 
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true)
-    )
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProvider(this).get(QuizViewModel::class.java)
+    }
 
-    private var currentIndex = 0
     private var toaster: Toast? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -48,29 +50,64 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
         }
 
         prevButton.setOnClickListener {
-            currentIndex = (currentIndex - 1) % questionBank.size
-            if (currentIndex < 0) currentIndex += questionBank.size
+            quizViewModel.moveToPrev()
             updateQuestion()
         }
 
         questionTextView.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
         }
 
         updateQuestion()
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
     private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
+        val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
 
-        val drawResource = when (currentIndex) {
+        // Log a message at DEBUG log level
+        Log.d(TAG, "Current question index: $quizViewModel.currentIndex")
+
+        try {
+            quizViewModel.questionBank[quizViewModel.currentIndex]
+        } catch (ex: ArrayIndexOutOfBoundsException) {
+            // Log a message at ERROR log level, along with an exception stack trace
+            Log.e(TAG, "Index was out of bounds", ex)
+        }
+
+
+        val drawResource = when (quizViewModel.currentIndex) {
             0 ->
                 R.drawable.australia
             1 ->
@@ -85,18 +122,55 @@ class MainActivity : AppCompatActivity() {
                 R.drawable.lakebaikal
         }
 
-        geoImageView?.setImageResource(drawResource)
+        geoImageView.setImageResource(drawResource)
+
+
+        if (quizViewModel.questionBank[quizViewModel.currentIndex].answered) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
-
+        val correctAnswer = quizViewModel.currentQuestionAnswer
+        quizViewModel.questionBank[quizViewModel.currentIndex].answered = true
+        quizViewModel.answeredQuestions++
         val messageResId = if (userAnswer == correctAnswer) {
+            quizViewModel.correctAnswers++
             R.string.correct_toast
         } else {
             R.string.incorrect_toast
         }
         showToast(messageResId)
+        setButton(false)
+
+        if (quizViewModel.answeredQuestions >= quizViewModel.questionBank.size) {
+            val text: String = getString(
+                R.string.quiz_results,
+                (quizViewModel.correctAnswers.toFloat() / quizViewModel.questionBank.size.toFloat() * 100),
+                quizViewModel.correctAnswers, quizViewModel.questionBank.size
+            )
+            showToast(text)
+        }
+    }
+
+    private fun setButton(enabled: Boolean) {
+        trueButton.isEnabled = enabled
+        falseButton.isEnabled = enabled
+    }
+
+    private fun showToast(text: String) {
+        if (toaster != null) {
+            toaster?.cancel()
+        }
+        toaster = Toast.makeText(
+            this,
+            text,
+            Toast.LENGTH_LONG
+        )
+        toaster?.setGravity(Gravity.TOP, 0, 200)
+        toaster?.show()
     }
 
     private fun showToast(text: Int) {
---
+++ app/src/main/java/com/example/geoquiz/QuizViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.geoquiz
+
+import androidx.lifecycle.ViewModel
+
+
+class QuizViewModel : ViewModel() {
+
+    val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+    var currentIndex = 0
+    var correctAnswers = 0
+    var answeredQuestions = 0
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+
+    fun moveToPrev() {
+        currentIndex = (currentIndex - 1) % questionBank.size
+        if (currentIndex < 0) currentIndex += questionBank.size
+    }
+}
